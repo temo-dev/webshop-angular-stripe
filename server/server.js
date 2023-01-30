2f57dbe2e03fe966e079732956cacfa98e71802d
TuanAnh-Seta <anhnt6879@seta-international.vn>
2023-01-30 22:13:50 +0100

    final

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
